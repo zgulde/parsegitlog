155e9904f8837123b5daeed8a4ebc17f1db01892
Zach Gulde <zachgulde@gmail.com>
2021-03-27 11:17:26 -0500

    Improve cli

--- parsegitlog/__main__.py
+++ parsegitlog/__main__.py
@@ -1,8 +1,14 @@
 import json
 from parsegitlog.gitlog import get_commits
 import sys
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="python -m parsegitlog")
+    parser.add_argument("repo_path")
+    parser.add_argument("-i", "--indent", type=int, default=2)
+    args = parser.parse_args()
 
-    output = get_commits(sys.argv[1])
-    print(json.dumps(output, indent=2))
+    output = get_commits(args.repo_path)
+    indent = args.indent if args.indent > 0 else None
+    print(json.dumps(output, indent=indent))
